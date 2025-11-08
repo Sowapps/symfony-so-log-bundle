643b0c0c1e9829746707e875b5cf21230b40e97c
Florent HAZARD <f.hazard@sowapps.com>
2025-11-08 10:34:51 +0100

    ⬆️ Fix deps for upgrade sf 6.4/7.*

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+	->withPhpSets() // Reach your current PHP version
+	->withComposerBased(twig: true, doctrine:true, symfony: true)
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
